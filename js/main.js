9360cae82cd86ef416726e3b5db19b3a039e6616
Francisco Garcia <garciafran@hotmail.com>
2020-06-24 12:13:43 -0500

    Funciones puras y no puras

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ const compose = (...functions) => data =>
 
   const double = (x) => x*2;
 
-  console.log(double);
+console.log(double(2));
+
+const time = () => new Date().toLocaleTimeString()
+
+console.log(time());
+
+const isGreaterThan = (value, comparison) => value > comparison
+
+console.log(isGreaterThan(8,6))
